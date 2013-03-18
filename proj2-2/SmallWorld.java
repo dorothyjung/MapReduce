d1e91810b29f8502dffed66823acc61631b8df1a
Jene Li <yami280@berkeley.edu>
2013-03-18 13:54:27 -0700

    bugfix

--- proj2-2/SmallWorld.java
+++ proj2-2/SmallWorld.java
@@ -295,7 +295,7 @@ public class SmallWorld {
         Job job = new Job(conf, "load graph");
 
         job.setJarByClass(SmallWorld.class);
-        job.setNumReduceTasks(24);
+        job.setNumReduceTasks(24); // CHANGE to 4*Number of machines
 
         job.setMapOutputKeyClass(LongWritable.class);
         job.setMapOutputValueClass(LongWritable.class);
@@ -320,7 +320,7 @@ public class SmallWorld {
         while (i < MAX_ITERATIONS) {
             job = new Job(conf, "bfs" + i);
             job.setJarByClass(SmallWorld.class);
-            job.setNumReduceTasks(24) // CHANGE to 4*Number of Machinese
+            job.setNumReduceTasks(24); // CHANGE to 4*Number of Machines
 
             // Feel free to modify these four lines as necessary:
             job.setMapOutputKeyClass(LongWritable.class);
